5719f78c58987f9d9b1c137b518cabab06c59f95
muhammednazeer <nazeer4u2c@gmail.com>
2022-03-09 21:31:40 +0100

    create movie api key

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = 'http//www.omdbapi.com?apikey=18fcde43';
+const App = () => {
+
+   
+    return (
+       
+            <h1>Welcome to Film Empire</h1>
+       
+    );
 }
 
 export default App;
