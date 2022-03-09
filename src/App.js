946e0555572934af521b79c39b29a194b1b9d8b9
muhammednazeer <nazeer4u2c@gmail.com>
2022-03-09 21:33:19 +0100

    create a searchMovie function that will request movie title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React, {useEffect} from "react";
 const API_URL = 'http//www.omdbapi.com?apikey=18fcde43';
 const App = () => {
 
-   
+    const searchMovie = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data);
+    }
+  
     return (
        
             <h1>Welcome to Film Empire</h1>
